0d895fd2442d23e289040bbec27d4baa9779256f
Mahmoud Abduljawad <mahmoud@masaar.com>
2020-04-16 14:31:47 +0000

    Introduced Diff.format_doc_oper to prefix Doc Oper with underscores

--- modules/core/diff.py
+++ modules/core/diff.py
@@ -1,6 +1,6 @@
 from base_module import BaseModule
 from enums import Event
-from classes import ATTR, PERM
+from classes import ATTR, PERM, LIMP_DOC
 from config import Config
 
 from bson import ObjectId
@@ -8,6 +8,7 @@ from bson import ObjectId
 
 class Diff(BaseModule):
 	'''`Diff` module provides data type and controller for `Diff Workflow`. It is meant for use by internal calls only. Best practice to accessing diff docs is by creating proxy modules or writing LIMP methods that expose the diff docs.'''
+
 	collection = 'diff'
 	attrs = {
 		'user': ATTR.ID(desc='`_id` of `User` doc the doc belongs to.'),
@@ -15,10 +16,14 @@ class Diff(BaseModule):
 		'doc': ATTR.ID(desc='`_id` of the original doc.'),
 		'vars': ATTR.DICT(
 			desc='Key-value `dict` containing all attrs that have been updated from the original doc.',
-			dict={'__key': ATTR.STR(), '__val': ATTR.ANY()}
+			dict={'__key': ATTR.STR(), '__val': ATTR.ANY()},
 		),
-		'remarks': ATTR.STR(desc='Human-readable remarks of the doc. This is introduced to allow developers to add log messages to diff docs.'),
-		'create_time': ATTR.DATETIME(desc='Python `datetime` ISO format of the doc creation.'),
+		'remarks': ATTR.STR(
+			desc='Human-readable remarks of the doc. This is introduced to allow developers to add log messages to diff docs.'
+		),
+		'create_time': ATTR.DATETIME(
+			desc='Python `datetime` ISO format of the doc creation.'
+		),
 	}
 	defaults = {'doc': None, 'remarks': ''}
 	methods = {
@@ -28,6 +33,8 @@ class Diff(BaseModule):
 	}
 
 	async def pre_create(self, skip_events, env, query, doc, payload):
+		# [DOC] format Doc Oper with prefixed underscores to avoid data errors
+		doc = self.format_doc_oper(doc=doc)
 		# [DOC] Detect non-_id update query:
 		if '_id' not in query:
 			results = await Config.modules[doc['module']].read(
@@ -52,3 +59,15 @@ class Diff(BaseModule):
 			query['_id'][0] = query['_id'][0][-1]
 		doc['doc'] = ObjectId(query['_id'][0])
 		return (skip_events, env, query, doc, payload)
+
+	def format_doc_oper(self, *, doc: LIMP_DOC):
+		shadow_doc: LIMP_DOC = {}
+		for attr in doc.keys():
+			if attr[0] == '$':
+				shadow_doc[f'__{attr}'] = doc[attr]
+			elif type(doc[attr]) == dict:
+				shadow_doc[attr] = self.format_doc_oper(doc=doc[attr])
+			else:
+				shadow_doc[attr] = doc[attr]
+		return shadow_doc
+
