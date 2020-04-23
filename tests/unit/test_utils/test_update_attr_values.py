53502f102ff850c3d212312d7d19cc3b2a8534b5
Mahmoud Abduljawad <mahmoud@masaar.com>
2020-04-23 05:50:49 +0000

    Updated update_attr_values utility tests

--- tests/unit/test_utils/test_update_attr_values.py
+++ tests/unit/test_utils/test_update_attr_values.py
@@ -1,9 +1,22 @@
 from ....classes import ATTR
 from ....utils import update_attr_values
 
-import pytest
-
 def test_update_attr_values_default_dict():
 	attr = ATTR.DICT(dict={'key':ATTR.STR()})
-	update_attr_values(attr=attr, value='default', value_path='key', value_val=None)
-	assert attr._args['dict']['key']._default == None
+	update_attr_values(attr=ATTR.DICT(dict={'attr':attr}), value='default', value_path='attr.key', value_val='test_update_attr_values')
+	assert attr._args['dict']['key']._default == 'test_update_attr_values'
+
+def test_update_attr_values_default_list():
+	attr = ATTR.LIST(list=[ATTR.STR()])
+	update_attr_values(attr=ATTR.DICT(dict={'attr':attr}), value='default', value_path='attr:0', value_val='test_update_attr_values')
+	assert attr._args['list'][0]._default == 'test_update_attr_values'
+
+def test_update_attr_values_default_dict_nested_list():
+	attr = ATTR.DICT(dict={'key':ATTR.LIST(list=[ATTR.STR()])})
+	update_attr_values(attr=ATTR.DICT(dict={'attr':attr}), value='default', value_path='attr.key:0', value_val='test_update_attr_values')
+	assert attr._args['dict']['key']._args['list'][0]._default == 'test_update_attr_values'
+
+def test_update_attr_values_default_list_dict():
+	attr = ATTR.LIST(list=[ATTR.DICT(dict={'key':ATTR.STR()})])
+	update_attr_values(attr=ATTR.DICT(dict={'attr':attr}), value='default', value_path='attr:0.key', value_val='test_update_attr_values')
+	assert attr._args['list'][0]._args['dict']['key']._default == 'test_update_attr_values'
