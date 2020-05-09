75c315008617581506b8012b3338799aa85c5e8a
Mahmoud Abduljawad <mahmoud@masaar.com>
2020-05-09 08:37:06 +0000

    Updated Package Setup

--- setup.py
+++ setup.py
@@ -1,7 +1,10 @@
 import setuptools
 
-with open('README.md', 'r') as fh:
-	long_description = fh.read()
+with open('README.md', 'r') as f:
+	long_description = f.read()
+
+with open('requirements.txt' , 'r') as f:
+	requirements = f.readlines()
 
 setuptools.setup(
 	name='LIMP',
@@ -12,11 +15,21 @@ setuptools.setup(
 	long_description=long_description,
 	long_description_content_type='text/markdown',
 	url='https://github.com/masaar/limp',
+	project_urls={
+        'Docs: Github': 'https://github.com/masaar/limp-docs',
+        'GitHub: issues': 'https://github.com/masaar/limp/issues',
+        'GitHub: repo': 'https://github.com/masaar/limp',
+    },
 	packages=['limp', 'limp.packages', 'limp.packages.core'],
 	classifiers=[
 		'Programming Language :: Python :: 3',
-		'License :: OSI Approved :: LGPL-3.0 License',
+		'Programming Language :: Python :: 3.8',
+		'Development Status :: 5 - Production/Stable',
+		'License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)',
 		'Operating System :: OS Independent',
+		'Topic :: Internet :: WWW/HTTP',
+		'Framework :: AsyncIO',
 	],
 	python_requires='>=3.8',
+	install_requires=requirements,
 )
