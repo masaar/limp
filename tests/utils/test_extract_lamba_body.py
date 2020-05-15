ce0b4a4d48f754ec5ff6eb94781596b9c3151b9e
Mahmoud Abduljawad <mahmoud@masaar.com>
2020-05-15 17:06:26 +0400

    Updated Pytest Folder Structure

--- tests/utils/test_extract_lamba_body.py
+++ tests/utils/test_extract_lamba_body.py
@@ -1,13 +1,13 @@
 from limp.utils import extract_lambda_body
 
 
-def test_extract_lamba_body_inline():
+def test_extract_lambda_body_inline():
 	lambda_func = lambda x: x * 2
 	lambda_func_body = extract_lambda_body(lambda_func=lambda_func)
 	assert lambda_func_body == 'lambda_func = lambda x: x * 2'
 
 
-def test_extract_lamba_body_obj():
+def test_extract_lambda_body_obj():
 	obj = {
 		'lambda_func': lambda x: x * 2,
 	}
